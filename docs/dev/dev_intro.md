a2a23dcea0ad5163ff23e6d3e720f603058237f5
Lexinor <alternonolimit@gmail.com>
2022-10-14 09:04:10 +0200

    Encore des modifs de la doc

--- docs/dev/dev_intro.md
+++ docs/dev/dev_intro.md
@@ -1,6 +1,6 @@
 ---
 sidebar_position: 1
-title: Doc Dév
+title: Introduction Dev
 ---
 
 # Documentation Dev.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,15 +37,15 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/NewDawn-RP/newdawn-doc/',
+            'https://github.com/NewDawn-RP/newdawn-doc/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/NewDawn-RP/newdawn-doc/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/NewDawn-RP/newdawn-doc/tree/main/',
+        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
